451348d5c91cb6381c060a79eedeb9c0c4626bb9
haginus <hagiandrei.ah@gmail.com>
2021-01-06 15:37:31 +0200

    Add project files.

---
+++ Biblioteca/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Biblioteca.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Biblioteca.Startup))]
+namespace Biblioteca
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateUserRoles();
+        }
+
+        private void CreateUserRoles()
+        {
+            var ctx = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ctx));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
+
+            if(!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole("Admin");
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole("Customer");
+                roleManager.Create(role);
+            }
+        }
+    }
+}
